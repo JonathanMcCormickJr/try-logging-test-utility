ece78a4f788672c2cac70ad746e8d48062b17ddc
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-22 05:12:28 +0000

    Use logging for noting exceptions.

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ logging.info('This is an info message')
 logging.warning('This is a warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
+
+
+try:
+    # some code that may raise an exception
+    1/0
+except Exception as e:
+    logging.exception('An error occurred: {}'.format(str(e)))
